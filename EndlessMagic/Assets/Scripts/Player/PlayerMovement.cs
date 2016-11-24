a2e6fae01e5561396a24f24bd7c04a5676e5a9ae
Zain Stiffler <19stizai@hawken.edu>
2016-11-24 17:31:56 -0500

    Finished Anims for Walking, made bounds so player can't leave screen

--- EndlessMagic/Assets/Scripts/Player/PlayerMovement.cs
+++ EndlessMagic/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
     Animator anim;
     Rigidbody2D rd;
-    public int speed;
+    public float speed;
 	// Use this for initialization
 	void Start () {
         anim = this.gameObject.GetComponent<Animator>(); //Need animator component for setting correct animation during movement
@@ -15,24 +15,32 @@ public class PlayerMovement : MonoBehaviour {
 	void FixedUpdate () {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical"); //get inputs
-
-
+                                                // this.gameObject.transform.Translate(new Vector2(transform.position.x + (speed/10) * x, transform.position.y + (speed/10) * y)); //gets current position and translates in both x and y from it
+        rd.AddForce(new Vector2(speed * x, speed * y));
+        SetAnim(x, y);
 	}
-    internal void SetAnim(float x, float y) {
+    private void SetAnim(float x, float y) {
         if(x==0 && y == 0) {
             anim.SetBool("isStanding", true); //if both are 0 then he's standing, the anim controller setup decides which he goes to 
         }
         if(x != 0) {
             if (x == 1) {
                 anim.SetBool("Wright", true); //right then play right
-                if (y == 1) //trying to create illusion of both by playing the y right after quickly
-                    anim.SetBool("Wup", true);
-                if (y == -1)
-                    anim.SetBool("Wdown", true);
+                addYMove(y);
             }
             if (x == -1)
                 anim.SetBool("Wleft", true); //left then play left
+                addYMove(y);
+        } else
+        {
+            addYMove(y); //just move in a y direction
         }
 
+    }
+    private void addYMove(float y) {
+        if (y == 1) //trying to create illusion of both by playing the y right after quickly
+            anim.SetBool("Wup", true);
+        if (y == -1)
+            anim.SetBool("Wdown", true);
     }
 }
