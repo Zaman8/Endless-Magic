3c1b45b9e2c3beb6ec84bb40d9ada204b8988a8e
Zain Stiffler <19stizai@hawken.edu>
2016-11-26 11:41:53 -0500

    Changed Player size scale to 1 and added camera follow

---
+++ EndlessMagic/Assets/Scripts/Enviroment/CameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollower : MonoBehaviour {
+    public Transform target;
+    [SerializeField]
+    float smoothing;
+
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        
+	}
+}
