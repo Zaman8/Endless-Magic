ede6a08955759dcb5508d7b5cf27d26fbd7dcc11
Zain Stiffler <19stizai@hawken.edu>
2016-12-26 13:41:14 -0500

    Changing funds about Enemy Movement and detections

--- EndlessMagic/Assets/Scripts/Enemy/EnemyMovement.cs
+++ EndlessMagic/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Quick : MonoBehaviour {
-    Rigidbody2D rd; 
+public class EnemyMovement : MonoBehaviour {
+
 	// Use this for initialization
 	void Start () {
-        rd = GetComponent<Rigidbody2D>();
-        rd.AddForce(new Vector2(0, 15));
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    public void AvoidProjectile(Vector3 pPosition, float distance)
+    {
+
+    }
 }
---
+++ EndlessMagic/Assets/Scripts/Helper Classes/MissileWarning.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileWarning : MonoBehaviour {
+    GameObject EnemyInPath = null;
+    float distance;
+    Ray probe;
+    RaycastHit hit;
+    // Update is called once per frame
+    void FixedUpdate () {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+       
+        probe.origin = transform.position;
+        probe.direction = transform.forward;
+        if (Physics.Raycast(probe, out hit))
+        {
+            if (hit.collider.gameObject.CompareTag("Enemy"))
+            {
+                EnemyInPath = hit.collider.gameObject;
+                distance = hit.distance;
+                WarnEnemy();
+            }
+        }
+	}
+
+    void WarnEnemy()
+    {
+       EnemyMovement eMove = EnemyInPath.GetComponent<EnemyMovement>();
+        eMove.AvoidProjectile(transform.position, distance);
+    }
+}
